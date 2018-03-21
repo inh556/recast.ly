8aa781d3530937723766a964191ccef6d0e7e509
inh556 <yanbin.jin@gmail.com>
2018-03-21 15:34:00 -0700

    Made videoList render with correct thumbnails, title, descriptions

--- src/index.js
+++ src/index.js
@@ -1,2 +1,7 @@
-// TODO: Render the `App` component to the DOM
-
+ReactDOM.render(
+  <App />, document.getElementById('app')
+);
+ReactDOM.render(
+  <VideoList videos={exampleVideoData} />,
+  document.getElementsByClassName('col-md-5')[0]
+);
