d04db1fe48d2dc8010c870f13233bb81ff35f9b4
inh556 <yanbin.jin@gmail.com>
2018-03-21 15:55:35 -0700

    Add videoPlayer, refactor App to be a class component

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 ReactDOM.render(
-  <App />, document.getElementById('app')
+  <App />, 
+  document.getElementById('app')
 );
 ReactDOM.render(
   <VideoList videos={exampleVideoData} />,
   document.getElementsByClassName('col-md-5')[0]
+);
+
+ReactDOM.render(
+  <VideoPlayer video={exampleVideoData[0]} />,
+  document.getElementsByClassName('col-md-7')[0]
 );
